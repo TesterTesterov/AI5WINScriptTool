beafa46765b186c3a6371cbecf8720f5a83b83d0
Tester <66121918+TesterTesterov@users.noreply.github.com>
2021-12-30 11:08:48 +0000

    Fix

--- setup.py
+++ setup.py
@@ -8,16 +8,14 @@ if (sys.platform == 'win32'):
 
 
 executables = [cx_Freeze.Executable("main.py",
-                                    shortcut_name="SilkyArcTool",
-                                    shortcut_dir="SilkyArcTool",
-                                    #base="Win32GUI"
-                                    )]
+                                    shortcut_name="AI5WINScriptTool",
+                                    shortcut_dir="AI5WINScriptTool")]
 
 cx_Freeze.setup(
-        name="AI5WINArcTool",
-        version="2.1",
-        description="Dual languaged (rus+eng) tool for packing and unpacking archives of Silky Engine.\n"
-                    "Двуязычное средство (рус+англ) для распаковки и запаковки архивов Silky Engine.",
+        name="AI5WINScriptTool",
+        version="1.0",
+        description="Dual languaged (rus+eng) tool for compiling and decompiling mes scripts of AI5WIN.\n"
+                    "Двухязычное средство (рус+англ) для компиляции и декомпиляции скриптов mes AI5WIN.",
         options={"build_exe": {"packages": []}},
         executables=executables
 )
